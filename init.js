ea39032592a841a07d4ed4a647a8e614781961cc
Grant Cheng <chengxgrant@gmail.com>
2022-05-03 15:30:02 -0700

    Added lots of features
    Got rid of line charts
    Added logarithmic scaling
    Added proportional scaling
    Added legend
    Sorted stacked bar graphs

--- init.js
+++ init.js
@@ -1,18 +1,19 @@
-let DATA_PER_POS = 6;
+let DATA_PER_POS = 5;
 // let DATA_SIZE = 0;
 const COUNT = "count";
+const LOG = "log";
 const PROPORTION = "proportion";
-const BARS = "bars";
-const CURVES = "curves";
+// const BARS = "bars";
+// const CURVES = "curves";
 const BOUNDS = {
     low: 0,
     high: 0,
 };
-const TYPE_CODES = {
-    A: 0,
-    G: 1,
-    C: 2,
-    T: 3,
-    OTHER: 4,
-    TOTAL: 5,
-};
+// const TYPE_CODES = {
+//     A: 0,
+//     G: 1,
+//     C: 2,
+//     T: 3,
+//     OTHER: 4,
+//     TOTAL: 5,
+// };
