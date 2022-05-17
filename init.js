b8379de4ae95c5d6c44eb7f97005150122a63321
Grant Cheng <chengxgrant@gmail.com>
2022-05-17 15:05:27 -0700

    Clean up directories pt. 1

--- init.js
+++ init.js
@@ -1,12 +1,9 @@
 let DATA_PER_POS = 5;
-// let DATA_SIZE = 0;
 const COUNT = "count";
 const LOG = "log";
 const PROPORTION = "proportion";
 const FAS = "fasta";
-const SBV = "sambamviz"
-// const BARS = "bars";
-// const CURVES = "curves";
+const SBV = "sambamviz";
 const BOUNDS = {
     low: 0,
     high: -1,
@@ -17,15 +14,7 @@ const COLORS = {
     G: '#EB413C',
     T: '#3C89EE',
     N: '#000000'
-}
-// const TYPE_CODES = {
-//     A: 0,
-//     G: 1,
-//     C: 2,
-//     T: 3,
-//     OTHER: 4,
-//     TOTAL: 5,
-// };
+};
 let tsvString;
 let genSeq = [];
 let tsvDataCount = [];
