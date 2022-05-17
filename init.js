246b12140cee6fd500f4d8f9d43461d89c7ec8cd
Grant Cheng <chengxgrant@gmail.com>
2022-05-17 15:56:46 -0700

    Organized code, added edge cases

--- init.js
+++ init.js
@@ -1,13 +1,19 @@
-let DATA_PER_POS = 5;
+const DATA_PER_POS = 5;
 const COUNT = "count";
 const LOG = "log";
 const PROPORTION = "proportion";
 const FAS = "fasta";
 const SBV = "sambamviz";
+
+const CHECK_BOUNDS_ERR = "Please make sure bounds are valid."
+const CHECK_FILES_ERR = "Please make sure file types are correct."
+const CHECK_DATA_ERR = "Please make sure that file data is corect."
+
 const BOUNDS = {
     low: 0,
     high: -1,
 };
+
 const COLORS = {
     A: '#65F73E',
     C: '#FFB340',
@@ -15,6 +21,12 @@ const COLORS = {
     T: '#3C89EE',
     N: '#000000'
 };
+
+const FILE_STATUS = {
+    sbv: false,
+    fas: false
+}
+
 let tsvString;
 let genSeq = [];
 let tsvDataCount = [];
---
+++ main.js
@@ -0,0 +1,162 @@
+document.getElementById('upload-files').addEventListener('click', () => {
+    generateData();
+
+    let lowerDataBound = BOUNDS.low;
+    let upperDataBound = BOUNDS.high;
+    loadSBV(lowerDataBound, upperDataBound, tsvDataCount, tsvDataProp);
+});
+
+document
+    .getElementById('sambamviz-upload')
+    .addEventListener('change', () => {
+        processFiles(SBV);
+    });
+
+document
+    .getElementById('fasta-upload')
+    .addEventListener('change', () => {
+        processFiles(FAS);
+    });
+
+document.querySelectorAll('.color-picker').forEach((input) => {
+    input.addEventListener('change', () => {
+        setVegaScheme();
+    });
+});
+
+document.getElementById('loadGraph').addEventListener('click', () => {
+    let lowerDataBound = BOUNDS.low;
+    let upperDataBound = BOUNDS.high;
+
+    lowerDataBound = parseInt(document.getElementById('lower-bound').value);
+    upperDataBound =
+        parseInt(document.getElementById('upper-bound').value) + 1;
+    loadSBV(lowerDataBound, upperDataBound, tsvDataCount, tsvDataProp);
+});
+
+async function generateData() {
+    try {
+        await tsvToArr(tsvString);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function processFiles(fileType) {
+    try {
+        if (fileType === SBV) {
+            const sbvFile =
+                document.getElementById('sambamviz-upload').files[0];
+
+            if (sbvFile.name.split(".")[1] === "tsv") {
+                FILE_STATUS.sbv = true;
+                await processSamBamViz(sbvFile);
+            } else {
+                FILE_STATUS.sbv = false;
+            }
+        }
+
+        if (fileType === FAS) {
+            const fasFile = document.getElementById('fasta-upload').files[0];
+
+            if (fasFile.name.split(".")[1] === "fas") {
+                FILE_STATUS.fas = true;
+                await processFasta(fasFile);
+            } else {
+                FILE_STATUS.fas = false;
+            }
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function processSamBamViz(file) {
+    return new Promise((resolve, reject) => {
+        let reader = new FileReader();
+
+        reader.onload = () => {
+            tsvString = reader.result;
+            resolve('done');
+        };
+
+        reader.readAsText(file);
+    });
+}
+
+function processFasta(file) {
+    return new Promise((resolve, reject) => {
+        let reader = new FileReader();
+
+        reader.onload = () => {
+            genSeq = [];
+            let genSeqStr = reader.result;
+            for (
+                let i = genSeqStr.indexOf('\n') + 1;
+                i < genSeqStr.length;
+                i++
+            ) {
+                if (genSeqStr[i] != '\n') {
+                    genSeq.push(genSeqStr[i]);
+                }
+            }
+            resolve('done');
+        };
+
+        reader.readAsText(file);
+    });
+}
+
+function setVegaScheme() {
+    COLORS.A = document.getElementById('a-color').value;
+    COLORS.C = document.getElementById('c-color').value;
+    COLORS.G = document.getElementById('g-color').value;
+    COLORS.T = document.getElementById('t-color').value;
+    COLORS.N = document.getElementById('n-color').value;
+}
+
+function checkIssues(lowBounds, highBounds) {
+    if (!FILE_STATUS.sbv || !FILE_STATUS.fas) {
+        return CHECK_FILES_ERR;
+    }
+
+    if (lowBounds > highBounds) {
+        return CHECK_BOUNDS_ERR;
+    }
+
+    return "";
+}
+
+function loadSBV(lowerDataBound, upperDataBound, tsvData, tsvDataProp) {
+    let errorMessage = checkIssues(lowerDataBound, upperDataBound);
+    if (errorMessage) {
+        alert(errorMessage);
+        return;
+    }
+
+    vega.scheme('strands', [
+        COLORS.A,
+        COLORS.C,
+        COLORS.G,
+        COLORS.T,
+        COLORS.N
+    ]);
+
+    let countData = tsvData.slice(
+        lowerDataBound * DATA_PER_POS,
+        upperDataBound * DATA_PER_POS
+    );
+
+    let propData = tsvDataProp.slice(
+        lowerDataBound * DATA_PER_POS,
+        upperDataBound * DATA_PER_POS
+    );
+
+    if (document.getElementById('count-option').checked) {
+        loadBarGraph(countData, COUNT);
+    } else if (document.getElementById('log-option').checked) {
+        loadBarGraph(countData, LOG);
+    } else if (document.getElementById('proportion-option').checked) {
+        loadBarGraph(propData, PROPORTION);
+    }
+}
